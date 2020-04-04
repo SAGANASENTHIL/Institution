3110c5f511abc1438d71412dc2a74fe4c9d72516
SAGANASENTHIL <sagana20042000@gmail.com>
2020-04-04 19:45:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/institution/secondactivity.java
@@ -0,0 +1,20 @@
+package com.example.institution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class secondactivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondactivity);
+        textView=(TextView)findViewById(R.id.txtitem);
+        String Tempholder=getIntent().getStringExtra("Listviewclickvalue");
+
+        textView.setText(Tempholder);
+    }
+}
